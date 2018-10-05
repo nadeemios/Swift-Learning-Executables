939a0059b36e2651d18e97e83c75c4ae057edb77
Nadeem Ahmad <nadeem@digitalmettle.com>
2018-10-05 16:44:50 +0530

    Updated Package.swift

--- Sources/SwiftConceptsExecutable/main.swift
+++ Sources/SwiftConceptsExecutable/main.swift
@@ -1,1 +1,24 @@
+
+import Alamofire
+import Foundation
 print("Hello, world!")
+
+func display(){
+    // Alamofire 4
+    let parameters: Parameters = ["foo": "bar"]
+    
+    Alamofire.request("", method: .get, parameters: parameters, encoding: JSONEncoding.default)
+        .downloadProgress(queue: DispatchQueue.global(qos: .utility)) { progress in
+            print("Progress: \(progress.fractionCompleted)")
+        }
+        .validate { request, response, data in
+            // Custom evaluation closure now includes data (allows you to parse data to dig out error messages if necessary)
+            return .success
+        }
+        .responseJSON { response in
+            debugPrint(response)
+    }
+}
+
+
+display()
